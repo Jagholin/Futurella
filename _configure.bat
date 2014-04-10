d756e5db353eeb2f01e927bee6a77d0720b0cb04
Jagholin <pavel.belskiy@student.hpi.uni-potsdam.de>
2014-04-10 11:29:42 +0200

    Remove OSG dependencies, add Magnum search code to CMakeLists

--- _configure.bat
+++ _configure.bat
@@ -1,7 +1,3 @@
-set OSG_ROOT=C:\Program Files\OpenSceneGraph12
-set LIB_DIR=C:\libs\osg\3rdparty
 set BOOST_ROOT=C:\libs\boost_1_55_0
-set CEGUIDIR=C:\CeGUI\0.8.2
 set FMODDIR=C:\libs\FMOD
-set BULLET_ROOT=C:\libs\bullet-2.82 
-set PATH=%OSG_ROOT%\bin;%CEGUIDIR%\bin;%FMODDIR%\api;%PATH%
+set PATH=%FMODDIR%\api;%PATH%
